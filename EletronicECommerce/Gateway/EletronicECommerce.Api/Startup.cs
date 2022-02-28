cb5a6e954d8ae56c4d997f6777724e671bd1c9aa
AnderDev <gcosta19@gmail.com>
2022-02-28 17:08:06 -0300

    Refactor Identity config

--- EletronicECommerce/Gateway/EletronicECommerce.Api/Startup.cs
+++ EletronicECommerce/Gateway/EletronicECommerce.Api/Startup.cs
@@ -1,12 +1,10 @@
-using System.Text;
 using EletronicECommerce.Infrastructure.Config;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using EletronicECommerce.Infrastructure.Security;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 
 namespace EletronicECommerce.Api
 {
@@ -24,29 +22,15 @@ namespace EletronicECommerce.Api
 
             services.AddControllers();
 
-            services.AddAuthentication(opts => 
-            {
-                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.SecurityTokenKey)),
-                    ValidateIssuer = true,
-                    ValidateLifetime = true,
-                    ValidateAudience = false, 
-                    ValidIssuer = Settings.Issuer                
-                };
-            }); 
+            services.Authentication();
+
             services.SwaggerConfiguration();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwaggerConfiguration();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
